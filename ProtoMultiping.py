ffd4edb452a1b03d6207783e37afb0ead5c52ee5
Ivan C. Flores Tesmov <flores.ivan940214@gmail.com>
2023-08-16 17:34:00 -0600

    Prototipo del Proyecto Multiping - Primer commit

---
+++ ProtoMultiping.py
@@ -0,0 +1,14 @@
+'''Prototito personal, con posibilidad de implementar al proyecto original'''
+import os #Importar sistema operativo y obtener una ruta de trabajo
+
+with open("ip_lista.txt") as file: #Apertura del archivo de texto
+    dump = file.read() #Lectura 
+    dump = dump.splitlines() 
+    print(dump)
+    
+    for ip in dump:
+        print('ping -> IP: ', ip)
+        print('-'*60)
+        os.system('ping -n 2 {}'.format(ip)) #Ping a las direcciones IP importadas del archivo .txt
+        print('-'*60)
+    
