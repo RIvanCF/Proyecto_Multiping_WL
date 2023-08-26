324ce86b9b74e13fc00fd8b504b01ab51efaaf81
Ivan C. Flores Tesmov <flores.ivan940214@gmail.com>
2023-08-25 18:18:38 -0600

    Leves mejoras en el codigo, implementar boton de ejecución aun en proceso, codigo en VBA en excel aun en proceso

--- ProtoMultiping.py
+++ ProtoMultiping.py
@@ -14,18 +14,18 @@ for ip in dump:
     print('ping => IP: ', ip)#Impresion de la direccion ip a pingear
     print('-'*30)
     res=os.popen(f'ping -n 2 {ip}').read() #Ping y lectura a las direcciones IP importadas del archivo .txt 
-    
+
 
 #Discriminacion de direcciones IP segun su respuesta al ping
     if(("Tiempo de espera agotado " or "Host de destino inaccesible")) in res: #Condición 1
-        print(res) 
+        print(res)
         f=open("output.txt", "a") #importar la impresion al documento output.txt
         f.write(str(ip) + '  Desconectado' + '\n' + '-'*30 + '\n') #Imprimir el resultado si cumple con la 'Condicíon 1'
         f.close()
     else: #De no cumplirse la 'Condición 1'
         print(res)
         f=open("output.txt", "a")
-        f.write(str(ip) + '  Conectado' + '\n' + '-'*30 + '\n')
+        f.write(str(ip) + '  Conectado' + '\n' + '-'*30 + '\n' )
         f.close()
 
 #Imprimir resultados al documento 'output.txt' 
