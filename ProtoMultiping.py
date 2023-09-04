8bd5542dc4fd2fd047a39b0c0b083e170da198e4
Ivan C. Flores Tesmov <flores.ivan940214@gmail.com>
2023-09-04 09:50:46 -0600

    Se tiene una version BETA DE PRUEBA EN EL PROYECTO

--- ProtoMultiping.py
+++ ProtoMultiping.py
@@ -2,7 +2,7 @@
 import os #Importar sistema operativo y obtener una ruta de trabajo
 
 """Importar archivo .txt a la terminal"""
-with open("ip_lista.txt") as file: #Apertura del archivo de texto
+with open("IP_addresses.txt") as file: #Apertura del archivo de texto
     dump = file.read() #Lectura del archivo
     dump = dump.splitlines() 
     print(dump)#Imprimir datos contenidos del archivo .txt a la terminal
@@ -10,9 +10,7 @@ with open("ip_lista.txt") as file: #Apertura del archivo de texto
 """Ping automatico de las direcciones IP"""
 for ip in dump:
     os.system('cls')#Limpiar la pantalla por cada ping concluido 
-    print('-'*30)
     print('ping => IP: ', ip)#Impresion de la direccion ip a pingear
-    print('-'*30)
     res=os.popen(f'ping -n 2 {ip}').read() #Ping y lectura a las direcciones IP importadas del archivo .txt 
 
 
@@ -20,12 +18,12 @@ for ip in dump:
     if(("Tiempo de espera agotado " or "Host de destino inaccesible")) in res: #Condición 1
         print(res)
         f=open("output.txt", "a") #importar la impresion al documento output.txt
-        f.write(str(ip) + '  Desconectado' + '\n' + '-'*30 + '\n') #Imprimir el resultado si cumple con la 'Condicíon 1'
+        f.write(str(ip) + '  Desconectado' + '\n') #Imprimir el resultado si cumple con la 'Condicíon 1'
         f.close()
     else: #De no cumplirse la 'Condición 1'
         print(res)
         f=open("output.txt", "a")
-        f.write(str(ip) + '  Conectado' + '\n' + '-'*30 + '\n' )
+        f.write(str(ip) + '  Conectado' + '\n')
         f.close()
 
 #Imprimir resultados al documento 'output.txt' 
@@ -33,3 +31,5 @@ with open ("output.txt") as file:
     output = file.read()
     print(output)
 
+with open("output.txt", "w") as file:
+    pass
