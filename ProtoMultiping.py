16b93e5c87ba0f162429ca09cb49aceaae183eb4
Ivan C. Flores Tesmov <flores.ivan940214@gmail.com>
2023-09-06 13:27:47 -0600

    Actualización: Boton de refrescado implementado con boton stop

--- ProtoMultiping.py
+++ ProtoMultiping.py
@@ -12,19 +12,19 @@ for ip in dump:
     '''os.system('cls')'''#Limpiar la pantalla por cada ping concluido 
     print('-'*40 + '\n')
     print('ping => IP: ', ip)#Impresion de la direccion ip a pingear
-    res=os.popen(f'ping -n 2 {ip}').read() #Ping y lectura a las direcciones IP importadas del archivo .txt 
+    res=os.popen(f'ping -n 4 {ip}').read() #Ping y lectura a las direcciones IP importadas del archivo .txt 
 
 
 #Discriminacion de direcciones IP segun su respuesta al ping
     if(("Tiempo de espera agotado " or "Host de destino inaccesible")) in res: #Condición 1
         print(res)
         f=open("output.txt", "a") #importar la impresion al documento output.txt
-        f.write(str(ip) + '  Desconectado' + '\n') #Imprimir el resultado si cumple con la 'Condicíon 1'
+        f.write(str(ip) + '  INESTABLE' + '\n') #Imprimir el resultado si cumple con la 'Condicíon 1'
         f.close()
     else: #De no cumplirse la 'Condición 1'
         print(res)
         f=open("output.txt", "a")
-        f.write(str(ip) + '  Conectado' + '\n')
+        f.write(str(ip) + '  ESTABLE' + '\n')
         f.close()
 
 #Imprimir resultados al documento 'output.txt' 
@@ -32,7 +32,7 @@ with open ("output.txt") as file:
     output = file.read()
     print(output)
 
-    time.sleep(60)
+    time.sleep(60) #Tiempo de visualización antes de borrarse el contenido 
 
-with open("output.txt", "w") as file:
+with open("output.txt", "w") as file: 
     pass
