3f522960e31915714ded2423284831991d7ba82f
Ivan C. Flores Tesmov <flores.ivan940214@gmail.com>
2023-09-20 18:31:25 -0600

    Version 4

--- ProtoMultiping.py
+++ ProtoMultiping.py
@@ -17,29 +17,34 @@ def ping_host(dump):
 """Ping automatico de las direcciones IP"""
 for ip in dump:
     '''os.system('cls')'''#Limpiar la pantalla por cada ping concluido 
-    print('-'*40 + '\n')
-    print('ping => IP: ', ip)#Impresion de la direccion ip a pingear
-    res=os.popen(f'ping -n 2 -w 1200 {ip}').read() #Ping y lectura a las direcciones IP importadas del archivo .txt
+    if(ip <= "0"):
+        print("esto vale cero, ahi muere")
+        break
+    else:
+        print('-'*40 + '\n')
+        print('ping => IP: ', ip)#Impresion de la direccion ip a pingear
+        res=os.popen(f'ping -n 1 -w 1200 {ip}').read() #Ping y lectura a las direcciones IP importadas del archivo .txt
     
 #Discriminacion de direcciones IP segun su respuesta al ping
-    if("Tiempo de espera agotado " or "inaccesible." or ping_host(ip) >= 1200.0) in res: #Condición 1
-        print(res)
-        f=open("output.txt", "a") #importar la impresion al documento output.txt
-        f.write(str(ip) + ' [ERROR]' + ' DOWN' + '\n' ) #Imprimir el resultado si cumple con la 'Condicíon 1'
-        f.close()
-    else: #De no cumplirse la 'Condición 1'
-        print(res)
-        f=open("output.txt", "a")
-        f.write(str(ip)  + ' [OK] ' + str(ping_host(ip)) + '\n') #Se imprime la IP, estado de conexión, velocidad de respuesta Máxima
-        f.close()
+        if("Tiempo de espera agotado " or "inaccesible." or ping_host(ip) >= 1200.0) in res: #Condición 1
+            print(res)
+            f=open("output.txt", "a") #importar la impresion al documento output.txt
+            f.write(str(ip) + ' [ERROR]' + ' DOWN' + '\n' ) #Imprimir el resultado si cumple con la 'Condicíon 1'
+            f.close()
+
+        else: #De no cumplirse la 'Condición 1'
+                print(res)
+                f=open("output.txt", "a")
+                f.write(str(ip)  + ' [OK] ' + str(ping_host(ip)) + '\n') #Se imprime la IP, estado de conexión, velocidad de respuesta Máxima 
+                f.close()
     
 #Imprimir resultados al documento 'output.txt' 
 with open ("output.txt") as file:
-    output = file.read()
-    print(output)
+        output = file.read()
+        print(output)
 
-    time.sleep(30) #Tiempo de visualización antes de borrarse el contenido 
+        time.sleep(10) #Tiempo de visualización antes de borrarse el contenido 
 
 with open("output.txt", "w") as file: 
-    pass
+        pass
     
