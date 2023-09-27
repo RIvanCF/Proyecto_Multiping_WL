24b5cd7d80cd2b13b3c28137931f796ac6b6dcb8
Ivan C. Flores Tesmov <flores.ivan940214@gmail.com>
2023-09-26 18:21:30 -0600

    Implementación a la conversion .exe

--- Uniping.py
+++ Uniping.py
@@ -10,7 +10,13 @@ with open("IP_addresses.txt") as file:
 def ping_host(dump):
     ping_result = ping(target = dump, count = 100, timeout=1.5, verbose=True)
 
-    return {'IP': dump, 'LATENCIA MINIMA': ping_result.rtt_min_ms, 'LATENCIA MÁXIMA': ping_result.rtt_max_ms, 'PAQUETES PERDIDOS': ping_result.packet_loss*100 }
+    return {
+        'IP': dump, 
+        'LATENCIA MINIMA': ping_result.rtt_min_ms, 
+        'LATENCIA MÁXIMA': ping_result.rtt_max_ms, 
+        'PAQUETES PERDIDOS': ping_result.packet_loss*100 
+    }
+
 
 '''
 for ip in dump:
@@ -28,4 +34,6 @@ with open("output.txt", "w") as file:
     pass 
 '''
 for ip in dump:
+    print('|','-'*30,' INFORMACIÓN DE LA DIRECCIÓN IP ',ip,'-'*30,'|','\n')
     print('\n',ping_host(ip),'\n')
+    print('|','-'*30," EL PROGRAMA FINALIZÓ SU EJECUCIÓN ",'-'*30,'|','\n')
